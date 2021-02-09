3f95d4039d5e58c3954c1a7bb9356447629ea977
Bradley Cantrell <cantrellbldev@gmail.com>
2021-02-09 17:47:42 -0500

    adds JS to listen for mousover and change the color of the div, removed placeholder text in the div for now

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-i=0
-for(i=0;i<16;i++){
+
+for(i=0;i<256;i++){ //256 because we have a 16 x 16 grid, this could be set as a variable above provided by the user
 
     const sketchpad = document.querySelector('.sketchpad'); //selects the .sketchpad div that already exists
 
@@ -8,8 +8,21 @@ for(i=0;i<16;i++){
  
     canvas.classList.add("canvas"); //adds the class "canvas" to the new <div>
    
-    canvas.textContent = "this is the canvas"; //adds the text "this is the canvas" to the new <div>
+    canvas.style.minWidth = "30px";
+    canvas.style.minHeight = "30px";
  
     sketchpad.appendChild(canvas); //appends the new div "canvas" to the existing div "sketchpad"
+
+    
 }
-   
+
+for(i=0;i<256;i++){
+
+    const paintedTile = document.getElementsByClassName("canvas")[i];
+    
+    paintedTile.onmouseover = function() {
+        paintedTile.classList.add("painted");
+
+    }
+
+}
