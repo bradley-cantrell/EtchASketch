f0e14452e95414f8d32cba096ffb0bbd79d8506c
bradley.cantrell <cantrellbldev@gmail.com>
2021-02-11 18:08:42 -0500

    added generate/clear buttons

--- app.js
+++ app.js
@@ -1,53 +1,39 @@
 
 //Creates a variable that selects our button
+const generateButton = document.getElementById("generate-button");
 const resetButton = document.getElementById("reset-button");
 
 //Prompts the user for input and converts that to the number of canvas divs that need to be made
-getSize();
-
-function getSize(){
+document.getElementById("generate-button").addEventListener("click", function(){
     var userInput = prompt("How many squares per side would you like your new grid to be?", "16");
     var inputToInt = parseInt(userInput);
     gridSize = inputToInt * inputToInt;
+  
     console.log(gridSize);
-}
-
-
-
-
-
-//Create grid, insert it into html with class canvas so it can be painted later
-
-
-
-for(i=0;i<gridSize;i++){ 
-
-    const sketchpad = document.querySelector('.sketchpad'); //selects the .sketchpad div that already exists
-
-    const canvas = document.createElement('div') //creates a new <div> and assigns it to "canvas"
- 
- 
-    canvas.classList.add("canvas"); //adds the class "canvas" to the new <div>
- 
-    sketchpad.appendChild(canvas); //appends the new div "canvas" to the existing div "sketchpad"
-
     
-}
+    for(i=0;i<gridSize;i++){ //generates the canvas
 
-
-
-
-
-
-//Paints a tile when it is hovered
-for(i=0;i<gridSize;i++){
-
-    const paintedTile = document.getElementsByClassName("canvas")[i]; //assigns the painted tiles to a variable
+        const sketchpad = document.querySelector('.sketchpad'); 
     
-    paintedTile.onmouseover = function() {
-        paintedTile.classList.add("painted"); //onclick applies the class "painted" which changes the color of the block
-
+        const canvas = document.createElement('div') 
+     
+     
+        canvas.classList.add("canvas"); 
+     
+        sketchpad.appendChild(canvas); 
+            
     }
+  
+    for(i=0;i<gridSize;i++){ //paints tiles on mouseover
+        const paintedTile = document.getElementsByClassName("canvas")[i]; 
+        
+        paintedTile.onmouseover = function() {
+            paintedTile.classList.add("painted"); 
+    
+    }}
 
-}
+        
+
+        
+})
 
