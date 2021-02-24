f101ed7a09d461b1d340735dc8d48f8e1c4b70ce
bradley.cantrell <cantrellbldev@gmail.com>
2021-02-24 17:38:39 -0500

    combine create and reset buttons, auto reset on click if a grid exists, no need for a reset button

--- app.js
+++ app.js
@@ -7,17 +7,23 @@ const sketchpad = document.querySelector('#sketchpad');
 //Prompts the user for input and converts that to the number of columns the grid needs
 
 creationButton.addEventListener("click", function(){
-    let userInput = prompt("How many squares per side would you like your new grid to be? (1-100)", "16");
+    let userInput = prompt("How many squares per side would you like your grid to be? Pick a number between 1 and 100.")
     gridColumns = parseInt(userInput);
     gridSize = gridColumns * gridColumns;
     let sketchpad = document.querySelector('#sketchpad'); 
    
 
     
-        if(sketchpad.style.gridTemplateColumns){
+        if(document.querySelector("div#canvas")){
             alert("Please reset your previous grid first! Click Reset Grid below.");
         }else if(gridColumns>0 && gridColumns<101){ //forces a number between 1 and 100
+            function removeAllChildNodes(parent) { //clears an existing grid if there is one
+                while (parent.firstChild) {
+                    parent.removeChild(parent.firstChild);
+                }
+            }
             
+            removeAllChildNodes(sketchpad);
             function layoutGrid(gridColumns) {
                 const sketchpad = document.getElementById('sketchpad')
                 sketchpad.style.gridTemplateColumns = `repeat(${gridColumns}, 1fr)`
@@ -49,20 +55,11 @@ creationButton.addEventListener("click", function(){
             
             }
         
-        }}else{  //throws an error if the number is not between 1 and 100
+        }}else{  //throws an error if the input is not a number between 1 and 100 or is blank
             alert("You must pick a number between 1 and 100.");
         }
         
 })
 
-resetButton.addEventListener("click", function(){ //clears existing grid
-    function removeAllChildNodes(parent) {
-        while (parent.firstChild) {
-            parent.removeChild(parent.firstChild);
-        }
-    }
-    
-    removeAllChildNodes(sketchpad);
-    
-})
+
 
