dcc6a72a9a5e0bbb3bb82aeff3d383cf6d88fc63
bradley.cantrell <cantrellbldev@gmail.com>
2021-02-25 16:03:07 -0500

    refactored how to apply color to the div, moved it to javascript and randomized it from ROYGBIV colors

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 //Creates variables that select our buttons
 const creationButton = document.getElementById("creation-button");
-const resetButton = document.getElementById("reset-button");
 const sketchpad = document.querySelector('#sketchpad');
 
 //Prompts the user for input and converts that to the number of columns the grid needs
@@ -17,6 +16,7 @@ creationButton.addEventListener("click", function(){
         if(document.querySelector("div#canvas")){
             alert("Please reset your previous grid first! Click Reset Grid below.");
         }else if(gridColumns>0 && gridColumns<101){ //forces a number between 1 and 100
+
             function removeAllChildNodes(parent) { //clears an existing grid if there is one
                 while (parent.firstChild) {
                     parent.removeChild(parent.firstChild);
@@ -24,6 +24,7 @@ creationButton.addEventListener("click", function(){
             }
             
             removeAllChildNodes(sketchpad);
+            
             function layoutGrid(gridColumns) {
                 const sketchpad = document.getElementById('sketchpad')
                 sketchpad.style.gridTemplateColumns = `repeat(${gridColumns}, 1fr)`
@@ -48,18 +49,15 @@ creationButton.addEventListener("click", function(){
         
             for(i=0;i<gridSize;i++){ //paints tiles on mouseover
                 let paintedTile = document.getElementsByClassName("canvas")[i]; 
-                
+                let paintColors = ["red", "yellow", "orange", "green", "blue", "violet", "indigo"];
+                let randomColor = paintColors[Math.floor(Math.random() * paintColors.length)]
+
                 paintedTile.onmouseover = function() {
-                    paintedTile.classList.add("painted"); 
-                    
-            
+                    paintedTile.style.backgroundColor = randomColor;
             }
         
         }}else{  //throws an error if the input is not a number between 1 and 100 or is blank
             alert("You must pick a number between 1 and 100.");
         }
-        
+
 })
-
-
-
