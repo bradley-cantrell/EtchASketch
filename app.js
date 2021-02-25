70136007558c2bef2961eff123ec7c1f5206c12f
bradley.cantrell <cantrellbldev@gmail.com>
2021-02-25 16:20:01 -0500

    cleaned up structure

--- app.js
+++ app.js
@@ -3,61 +3,48 @@
 const creationButton = document.getElementById("creation-button");
 const sketchpad = document.querySelector('#sketchpad');
 
+function removeAllChildNodes(parent) { //clears an existing grid if there is one
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function layoutGrid(gridColumns) { //creates the grid based on user input
+    const sketchpad = document.getElementById('sketchpad')
+    sketchpad.style.gridTemplateColumns = `repeat(${gridColumns}, 1fr)`
+    sketchpad.style.gridTemplateRows = `repeat(${gridColumns}, 1fr)`
+}
+
+
 //Prompts the user for input and converts that to the number of columns the grid needs
 
 creationButton.addEventListener("click", function(){
     let userInput = prompt("How many squares per side would you like your grid to be? Pick a number between 1 and 100.")
-    gridColumns = parseInt(userInput);
-    gridSize = gridColumns * gridColumns;
-    let sketchpad = document.querySelector('#sketchpad'); 
+    let gridColumns = parseInt(userInput);
+    let gridSize = gridColumns * gridColumns; 
    
-
-    
-        if(document.querySelector("div#canvas")){
-            alert("Please reset your previous grid first! Click Reset Grid below.");
-        }else if(gridColumns>0 && gridColumns<101){ //forces a number between 1 and 100
-
-            function removeAllChildNodes(parent) { //clears an existing grid if there is one
-                while (parent.firstChild) {
-                    parent.removeChild(parent.firstChild);
-                }
-            }
-            
+        if(gridColumns>0 && gridColumns<101){ //forces a number between 1 and 100
             removeAllChildNodes(sketchpad);
-            
-            function layoutGrid(gridColumns) {
-                const sketchpad = document.getElementById('sketchpad')
-                sketchpad.style.gridTemplateColumns = `repeat(${gridColumns}, 1fr)`
-                sketchpad.style.gridTemplateRows = `repeat(${gridColumns}, 1fr)`
-            }
-
-        
             layoutGrid(gridColumns);
 
-            for(i=0;i<gridSize;i++){ //generates the canvas that can be painted
-
-                
-            
+            for(i=0;i<gridSize;i++){ //generates the divs within the grid
                 const canvas = document.createElement('div') 
-            
-            
                 canvas.classList.add("canvas"); 
-            
-                sketchpad.appendChild(canvas); 
-                    
+                sketchpad.appendChild(canvas);    
             }
         
-            for(i=0;i<gridSize;i++){ //paints tiles on mouseover
+            for(i=0;i<gridSize;i++){ //paints divs on mouseover
                 let paintedTile = document.getElementsByClassName("canvas")[i]; 
                 let paintColors = ["red", "yellow", "orange", "green", "blue", "violet", "indigo"];
                 let randomColor = paintColors[Math.floor(Math.random() * paintColors.length)]
 
                 paintedTile.onmouseover = function() {
                     paintedTile.style.backgroundColor = randomColor;
-            }
+                    }
         
-        }}else{  //throws an error if the input is not a number between 1 and 100 or is blank
-            alert("You must pick a number between 1 and 100.");
+            }
+        }else{  //throws an error if the input is not a number between 1 and 100 or is blank
+            alert("You must pick a number between 1 and 100."); //hanldes unexpected input
         }
 
 })
