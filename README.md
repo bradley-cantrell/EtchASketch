854f0a6cc69beea860f29815bd4731678e841c01
bradley.cantrell <cantrellbldev@gmail.com>
2021-03-04 16:07:46 -0500

    cleaning up javascript function syntax, organizing code within files, comments for clarity

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # EtchASketch
 Etch-A-Sketch JS Project From The Odin Project
+
+This project combines several Javascript fundamentals that I have been learning recently. 
+    - Functions
+    - For loops
+    - If/else statements
+    - Variable scope
+
+It was a deep dive into DOM manipulation and was my first time using Javascript to set content that changes based on user input. 
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-//Creates variables that select our buttons
+//Creates variables that select the button and the blank canvas space
 const creationButton = document.getElementById("creation-button");
 const sketchpad = document.querySelector('#sketchpad');
 
@@ -9,8 +9,7 @@ function removeAllChildNodes(parent) { //clears an existing grid if there is one
     }
 }
 
-function layoutGrid(gridColumns) { //creates the grid based on user input
-    const sketchpad = document.getElementById('sketchpad')
+function layoutGrid(gridColumns) { //creates the grid based on user input gridColumns
     sketchpad.style.gridTemplateColumns = `repeat(${gridColumns}, 1fr)`
     sketchpad.style.gridTemplateRows = `repeat(${gridColumns}, 1fr)`
 }
@@ -18,16 +17,16 @@ function layoutGrid(gridColumns) { //creates the grid based on user input
 
 //Prompts the user for input and converts that to the number of columns the grid needs
 
-creationButton.addEventListener("click", function(){
-    let userInput = prompt("How many squares per side would you like your grid to be? Pick a number between 1 and 100.")
+creationButton.addEventListener("click", () => {
+    let userInput = prompt("How many squares per side would you like your grid to be? Pick a number between 1 and 100.", "50")
     let gridColumns = parseInt(userInput);
     let gridSize = gridColumns * gridColumns; 
    
-        if(gridColumns>0 && gridColumns<101){ //forces a number between 1 and 100
+        if(gridColumns>0 && gridColumns<101){ //forces a number between 1 and 100, clears any existing grid, lays out new grid
             removeAllChildNodes(sketchpad);
             layoutGrid(gridColumns);
 
-            for(i=0;i<gridSize;i++){ //generates the divs within the grid
+            for(i=0;i<gridSize;i++){ //generates the divs within the new grid
                 const canvas = document.createElement('div') 
                 canvas.classList.add("canvas"); 
                 sketchpad.appendChild(canvas);    
@@ -38,13 +37,11 @@ creationButton.addEventListener("click", function(){
                 let paintColors = ["red", "yellow", "orange", "green", "blue", "violet", "indigo"];
                 let randomColor = paintColors[Math.floor(Math.random() * paintColors.length)]
 
-                paintedTile.onmouseover = function() {
+                paintedTile.onmouseover = () => {
                     paintedTile.style.backgroundColor = randomColor;
                     }
-        
-            }
+                }
         }else{  //throws an error if the input is not a number between 1 and 100 or is blank
-            alert("You must pick a number between 1 and 100."); //hanldes unexpected input
+            alert("You must pick a number between 1 and 100."); 
         }
-
 })
